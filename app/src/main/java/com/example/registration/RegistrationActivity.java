f4b6ade41c7409edd2fb998151314523545143eb
Jane <ezhbikovskaya@gmail.com>
2021-09-29 20:56:26 +0700

    creating an interface

---
+++ app/src/main/java/com/example/registration/RegistrationActivity.java
@@ -0,0 +1,18 @@
+package com.example.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+    protected void Registration(View view){
+
+    }
+}
